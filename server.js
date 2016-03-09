54f7fe30525b0cdf272b8a630a57b5ca9a8aa36b
Nasir Malik <nasir.malik@uac.edu.au>
2016-03-09 16:39:57 +1100

    Testing routing

--- server.js
+++ server.js
@@ -1,52 +1,18 @@
-var express = require("express")
-	stylus = require("stylus"),
-	logger = require('morgan'),
-	bodyParser = require('body-parser'),
-	mongoose = require('mongoose');
-var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
+var port = 3031;
+var express = require('express');
 var app = express();
 
-function compile(str, path){
-	return stylus(str).set("filename", path);
-}
-
-app.set("views", __dirname + "/server/views");
-app.set("view engine", "jade");
-
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(stylus.middleware({
-	src: __dirname + "/public",
-	compile: compile
-}));
-app.use(express.static(__dirname + "/public"));
-
-mongoose.connect('mongodb://localhost/multi');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Error Connecting db...'));
-db.once('open', function callback(){
-	console.log('Connection successful...');
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+  res.send('hello world');
 });
 
-var messageSchema = mongoose.Schema({message: String});
-var Message = mongoose.model('Message', messageSchema);
-var mongoMesage;
-Message.findOne().exec(function(err, messageDoc){
-	mongoMesage = messageDoc.message;
+app.get('/example/multiple/download', function (req, res, next) {
+  console.log('the response will be sent by the next function ...');
+  next();
+}, function (req, res) {
+  res.download('D:\\Project data\\Fee Service\\temp\\up150904.csv');
 });
 
-app.get("/partials/:partialName", function(req, res){
-	res.render("partials/" + req.params.partialName);
-})
-app.get("*", function(req, res){
-	res.render("index", {
-		mongoMessage: mongoMesage
-	});
-})
-
-var port = 3030;
 app.listen(port);
-
-console.log("Server is up and running at http://localhost:" + port);
+console.log('Staring server http://localhost:' + port);
